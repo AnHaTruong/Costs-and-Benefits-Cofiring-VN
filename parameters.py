47805d9bf9223a5801ff5badf2acb0fe1a2c6765
Minh Ha-Duong <haduong@minh-XPS13>
2016-02-01 16:57:45 +0700

    Table 2 data in

--- parameters.py
+++ parameters.py
@@ -32,3 +32,17 @@ NinhBinh.CoalConsumption   =     0.42 # Mt/yr
 NinhBinh.CoalHeatValue     =    25.3  # Mj/kg
 NinhBinh.PlantEfficiency   =   21.77  # percent
 NinhBinh.BoilerEfficiency  =   81.61  # percent
+
+MongDuong1.CapitalCost     =     50       # USD/kW
+MongDuong1.StrawRequired   = 259107       # ton/year
+MongDuong1.CoalPrice       =     52.69    # USD/ton
+MongDuong1.StrawUnitCost   =     41.31    # USD/ton
+MongDuong1.FixOMCost       =     32.34    # USD/kW.year
+MongDuong1.VariableOMCost  =      0.6     # UScent/kWh
+
+NinhBinh.CapitalCost     =     100       # USD/kW
+NinhBinh.StrawRequired   =   53362       # ton/year
+NinhBinh.CoalPrice       =     83.83    # USD/ton
+NinhBinh.StrawUnitCost   =     38.15    # USD/ton
+NinhBinh.FixOMCost       =     32.34    # USD/kW.year
+NinhBinh.VariableOMCost  =      0.6     # UScent/kWh
