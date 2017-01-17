2a014ea8dfc000eb0ef8e6358ec277b687280880
Minh Ha-Duong <minh.haduong@gmail.com>
2017-01-17 09:05:55 +0700

    Update doctest

--- job.py
+++ job.py
@@ -29,9 +29,9 @@ def bm_collection_work(plant):
 
     >>> from parameters import *
     >>> bm_collection_work(MongDuong1)
-    35.991732848796104
+    31.453275005976963
     >>> bm_collection_work(NinhBinh)
-    7.412339467796653
+    5.5446671167748685
     """
     return biomass_required(plant) * work_hour_day / winder_capacity
 
@@ -41,9 +41,9 @@ def bm_transport_work(plant):
 
     >>> from parameters import *
     >>> bm_transport_work(MongDuong1)
-    4.6467612441644475
+    3.918564866790392
     >>> bm_transport_work(NinhBinh)
-    0.21275304488580102
+    0.1376436085135254
     """
     return number_of_truck(plant) * transport_time(plant)
 
@@ -53,9 +53,9 @@ def number_of_truck(plant):
 
     >>> from parameters import *
     >>> print_with_unit(number_of_truck, MongDuong1, '1/y')
-    12955.4 1/y
+    11321.7 1/y
     >>> print_with_unit(number_of_truck, NinhBinh, '1/y')
-    2668.1 1/y
+    1995.82 1/y
     """
     return biomass_required(plant) / truck_load
 
@@ -65,9 +65,9 @@ def transport_time(plant):
 
     >>> from parameters import *
     >>> print_with_unit(transport_time, MongDuong1, 'hr')
-    3.14414 hr
+    3.034 hr
     >>> print_with_unit(transport_time, NinhBinh, 'hr')
-    0.698997 hr
+    0.604554 hr
     """
     return collection_radius(plant) * 2 / truck_velocity
 
@@ -77,9 +77,9 @@ def om_work(plant):
 
     >>> from parameters import *
     >>> om_work(MongDuong1)
-    4.449007529089665
+    3.888
     >>> om_work(NinhBinh)
-    0.5133470225872689
+    0.38400000000000006
     """
     return power_generation(plant) * biomass_ratio * OM_hour_MWh
 
@@ -89,9 +89,9 @@ def cofiring_work(plant):
 
     >>> from parameters import *
     >>> cofiring_work(MongDuong1)
-    45.08750162205021
+    39.25983987276735
     >>> cofiring_work(NinhBinh)
-    8.138439535269724
+    6.066310725288394
     """
     return bm_collection_work(plant) + bm_transport_work(plant) + om_work(plant)
 
@@ -101,9 +101,9 @@ def benefit_bm_collection(plant):
 
     >>> from parameters import *
     >>> print_with_unit(benefit_bm_collection, MongDuong1, 'kUSD/y')
-    350.209 kUSD/y
+    306.049 kUSD/y
     >>> print_with_unit(benefit_bm_collection, NinhBinh, 'kUSD/y')
-    72.124 kUSD/y
+    53.9511 kUSD/y
     """
     return bm_collection_work(plant) * wage_bm_collect
 
@@ -113,9 +113,9 @@ def benefit_bm_transport(plant):
 
     >>> from parameters import *
     >>> print_with_unit(benefit_bm_transport, MongDuong1, 'kUSD/y')
-    45.2142 kUSD/y
+    38.1287 kUSD/y
     >>> print_with_unit(benefit_bm_transport, NinhBinh, 'kUSD/y')
-    2.07014 kUSD/y
+    1.33931 kUSD/y
     """
     return bm_transport_work(plant) * wage_bm_transport
 
@@ -125,9 +125,9 @@ def benefit_om(plant):
 
     >>> from parameters import *
     >>> print_with_unit(benefit_om, MongDuong1, 'kUSD/y')
-    65.13 kUSD/y
+    56.9173 kUSD/y
     >>> print_with_unit(benefit_om, NinhBinh, 'kUSD/y')
-    7.515 kUSD/y
+    5.62146 kUSD/y
     """
     return om_work(plant) * wage_operation_maintenance
 
@@ -137,9 +137,9 @@ def total_job_benefit(plant):
 
     >>> from parameters import *
     >>> print_with_unit(total_job_benefit, MongDuong1, 'kUSD/y')
-    460.553 kUSD/y
+    401.094 kUSD/y
     >>> print_with_unit(total_job_benefit, NinhBinh, 'kUSD/y')
-    81.7091 kUSD/y
+    60.9118 kUSD/y
     """
     return benefit_bm_collection(plant) + benefit_bm_transport(plant) + benefit_om(plant)
 
