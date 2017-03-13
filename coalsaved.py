6eb90ed38da9d992fe767ec5b4a9fe96fe0d074f
Minh Ha-Duong <minh.haduong@gmail.com>
2017-03-13 11:19:48 +0700

    Delete coal_import_saving()

--- coalsaved.py
+++ coalsaved.py
@@ -11,7 +11,7 @@
 """
 
 
-from parameters import biomass_ratio, coal_import_price
+from parameters import biomass_ratio
 from biomassrequired import gross_heat_input
 
 
@@ -23,15 +23,6 @@ def coal_saved(plant):
     mass = gross_heat_input(plant) * biomass_ratio / plant.coal.heat_value
     mass.display_unit = 't/y'
     return mass
-
-
-def coal_import_saving(plant):
-    """ Return the maximum benefit for trade balance from the amount of
-        avoided coal import (equivalence to coal saved per year) from co-firing
-        assuming that same amount of coal will be imported if there is no co-firing
-
-    """
-    return coal_saved(plant) * coal_import_price
 
 
 if __name__ == "__main__":
