e14c0a5c3e448ded07944a15c5b954ba8a90d00e
An Ha Truong <truonganha87@gmail.com>
2017-03-16 15:02:38 +0700

    Add comments in emission_new.py

--- emission_new.py
+++ emission_new.py
@@ -24,6 +24,7 @@ from natu.units import t, km, y
 
 zero_transport = v_zeros * t * km / y
 
+# Define objects
 MD_plant_stack = v_Emitter({'6b_coal': MongDuong1.coal_used},
                            emission_factor,
                            MD_controls)
@@ -58,6 +59,7 @@ MDCofire_field = v_Emitter({'Straw': (
                             )},
                            emission_factor)
 
+# Calculate emission reduction
 MD_plant_ER = MD_plant_stack.emissions()["Total"] - MDCofire_plant_stack.emissions()["Total"]
 
 MD_transport_CO2 = MD_transport.emissions()["Total"] - MDCofire_transport.emissions()["Total"]
@@ -71,19 +73,22 @@ MD_field_ER = MD_field.emissions()["Total"] - MDCofire_field.emissions()["Total"
 
 MD_total_ER = MD_plant_ER + MD_transport_ER + MD_field_ER
 
+# Calculate benefit from emission reduction
 MD_total_benefit = MD_total_ER * specific_cost
 display_as(MD_total_benefit, 'kUSD/y')
 
+# Create pandas DataFrame from emission reduction Series
 list_of_series = [MD_plant_ER, MD_transport_ER, MD_field_ER, MD_total_ER, MD_total_benefit]
 row = ['Plant', 'Transport', 'Field', 'Total', 'Benefit']
 MD_ER_table = pd.DataFrame(list_of_series, index=row)
 
+# Split benefit from GHG emission reduction and health benefit
 MD_ER_benefit = MD_total_benefit['CO2']
 MD_health_benefit = MD_total_benefit.drop('CO2').sum()
 
 
 # Ninh Binh
-
+# Define objects
 NB_plant_stack = v_Emitter({'4b_coal': NinhBinh.coal_used},
                            emission_factor,
                            NB_controls)
@@ -123,7 +128,7 @@ NBCofire_field = v_Emitter({'Straw': (
                             - NinhBinhCofire.biomass_used)},
                            emission_factor)
 
-
+# Calculate emission reduction
 NB_plant_ER = NB_plant_stack.emissions()["Total"] - NBCofire_plant_stack.emissions()["Total"]
 
 NB_transport_CO2 = NB_transport.emissions()["Total"] - NBCofire_transport.emissions()["Total"]
@@ -134,12 +139,15 @@ NB_transport_ER = NB_transport_CO2.append(NB_transport_pollutant)
 NB_field_ER = NB_field.emissions()["Total"] - NBCofire_field.emissions()["Total"]
 NB_total_ER = NB_plant_ER + NB_transport_ER + NB_field_ER
 
+# Calculate benefit from emission reduction
 NB_total_benefit = NB_total_ER * specific_cost
 display_as(NB_total_benefit, 'kUSD/y')
 
+# Create pandas DataFrame from emission reduction Series
 list_of_series = [NB_plant_ER, NB_transport_ER, NB_field_ER, NB_total_ER, NB_total_benefit]
 row = ['Plant', 'Transport', 'Field', 'Total', 'Benefit']
 NB_ER_table = pd.DataFrame(list_of_series, index=row)
 
+# Split benefit from GHG emission reduction and health benefit
 NB_ER_benefit = NB_total_benefit['CO2']
 NB_health_benefit = NB_total_benefit.drop('CO2').sum()
