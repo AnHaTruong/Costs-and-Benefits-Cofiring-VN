0a3cfb2e4373a19cd32ad128ab74c567f5ef7181
An Ha Truong <truonganha87@gmail.com>
2017-03-28 10:29:05 +0700

    Remove benefitaddup (change table 12).

--- table12-benefits.py
+++ table12-benefits.py
@@ -8,9 +8,8 @@
 #     Creative Commons Attribution-ShareAlike 4.0 International
 #
 #
-""" Print table for the present value of benefit from co-firing added up
-    in benefitaddup.py
-"""
+""" Print table for the present value of benefit from co-firing added up"""
+
 from init import time_horizon
 from parameters import discount_rate
 from parameters import MongDuong1, MongDuong1Cofire, NinhBinh, NinhBinhCofire
@@ -19,12 +18,6 @@ from parameters import winder_rental_cost, straw, specific_cost
 from parameters import winder_haul, truck_velocity, work_hour_day, truck_loading_time
 from parameters import truck_load, OM_hour_MWh, wage_bm_transport
 from parameters import wage_bm_collect, wage_operation_maintenance, wage_bm_loading
-
-from benefitaddup import benefit_add_up, total_benefit_addup
-from farmerincome import total_income_benefit
-from emission import emission_reduction_benefit, total_health_benefit
-
-from job import total_job_benefit
 
 print("Total benefit over", time_horizon, "years")
 print("Discounted at", discount_rate)
@@ -35,20 +28,17 @@ def print_benefit_add_up(plant, cofiringplant):
     print('')
     print(cofiringplant.name)
     print('-------------------')
-    row2 = '{:30}' + '{:20.0f}' + '{:20.0f}'
+    row2 = '{:30}' + '{:20.0f}'
     print(row2.format('Health',
-                      benefit_add_up(total_health_benefit, plant, cofiringplant),
                       cofiringplant.health_npv(discount_rate, specific_cost)
                       )
           )
     print(row2.format('Emission reduction',
-                      benefit_add_up(emission_reduction_benefit, plant, cofiringplant),
                       cofiringplant.CO2_npv(discount_rate, specific_cost)
                       )
           )
 
     print(row2.format('Jobs',
-                      benefit_add_up(total_job_benefit, plant, cofiringplant),
                       cofiringplant.wages_npv(discount_rate,
                                               work_hour_day,
                                               winder_haul,
@@ -63,15 +53,9 @@ def print_benefit_add_up(plant, cofiringplant):
                       )
           )
     print(row2.format('Farmer income',
-                      benefit_add_up(total_income_benefit, plant, cofiringplant),
                       cofiringplant.straw_supply.farm_npv(discount_rate,
                                                           winder_rental_cost,
                                                           straw.price)
-                      )
-          )
-    print(row2.format('Total',
-                      total_benefit_addup(plant, cofiringplant),
-                      float('NaN')
                       )
           )
 
