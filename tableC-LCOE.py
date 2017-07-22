44c11a0d39cff2df643816bc5b68802db227a8e4
Minh Ha-Duong <minh.haduong@gmail.com>
2017-07-22 20:57:57 +0700

    Presents Table C side by side

--- tableC-LCOE.py
+++ tableC-LCOE.py
@@ -14,52 +14,49 @@ from natu.numpy import npv
 from parameters import MongDuong1System, NinhBinhSystem
 from parameters import discount_rate, tax_rate, depreciation_period, feedin_tariff
 
-from PowerPlant import CofiringPlant
+
+def tableC(system, tariff):
+    def printRowInt(label, a, b):
+        print('{:30}{:8.0f}{:20.0f}'.format(label, a, b))
+
+    def printRowFloat(label, x, y):
+        print('{:30}{:8.4f}{:20.4f}'.format(label, x, y))
+
+    def printRowNPV(label, v, w):
+        printRowInt(label, npv(discount_rate, v), npv(discount_rate, w))
+
+    def printRowNPV_na(label, w):
+        print('{:43}{:20.0f}'.format(label, npv(discount_rate, w)))
+
+    print(system.plant.name)
+    print('{:30}{:30}{:20}'.format('', "Reference", "Cofiring"))
+    printRowInt("Investment", system.plant.capital, system.cofiring_plant.capital)
+    printRowNPV("Fuel cost", system.plant.fuel_cost(), system.cofiring_plant.fuel_cost())
+    printRowNPV("  Coal", system.plant.coal_cost(), system.cofiring_plant.coal_cost())
+    printRowNPV_na("  Biomass", system.cofiring_plant.biomass_cost)
+    printRowNPV_na("    transportation", system.transport_cost)
+    printRowNPV_na("    straw at field", system.biomass_value)
+    printRowNPV("O&M cost",
+                system.plant.operation_maintenance_cost(),
+                system.cofiring_plant.operation_maintenance_cost())
+    printRowNPV("  coal", system.plant.coal_om_cost(), system.cofiring_plant.coal_om_cost())
+    printRowNPV_na("  biomass", system.cofiring_plant.biomass_om_cost())
+    printRowNPV("Tax",
+                system.plant.income_tax(tariff, tax_rate, depreciation_period),
+                system.cofiring_plant.income_tax(tariff, tax_rate, depreciation_period))
+    printRowNPV("Sum of costs",
+                system.plant.cash_out(tariff, tax_rate, depreciation_period),
+                system.cofiring_plant.cash_out(tariff, tax_rate, depreciation_period))
+    printRowNPV("Electricity produced",
+                system.plant.power_generation,
+                system.cofiring_plant.power_generation)
+    printRowFloat("Levelized cost of electricity",
+                  system.plant.lcoe(tariff, discount_rate, tax_rate, depreciation_period),
+                  system.cofiring_plant.lcoe(tariff, discount_rate, tax_rate, depreciation_period))
 
 
-# TODO: Row compares plant and cofiring_plant
-def tableC(plant, tariff, system=None):
-    def printRowInt(label, quantity):
-        print('{:30}{:8.0f}'.format(label, quantity))
+tableC(MongDuong1System, feedin_tariff['MD'])
 
-    def printRowFloat(label, value):
-        print('{:30}{:8.4f}'.format(label, value))
-
-    def printRowNPV(label, vector):
-        printRowInt(label, npv(discount_rate, vector))
-
-    print("Levelized cost of electricity - " + plant.name + "\n")
-    printRowInt("Investment", plant.capital)
-    printRowNPV("Fuel cost", plant.fuel_cost())
-    if isinstance(plant, CofiringPlant):
-        printRowNPV("  Coal", plant.coal_cost())
-        # Assuming the rent sharing goes to farmer, see also Table B
-        printRowNPV("  Biomass", plant.biomass_cost)
-        printRowNPV("    transportation", system.transport_cost)
-        printRowNPV("    straw at field", system.biomass_value)
-    printRowNPV("O&M cost", plant.operation_maintenance_cost())
-    if isinstance(plant, CofiringPlant):
-        printRowNPV("  coal", plant.coal_om_cost())
-        printRowNPV("  biomass", plant.biomass_om_cost())
-    printRowNPV("Tax", plant.income_tax(tariff,
-                                        tax_rate,
-                                        depreciation_period))
-    printRowNPV("Sum of costs", plant.cash_out(tariff,
-                                               tax_rate,
-                                               depreciation_period))
-    printRowNPV("Electricity produced", plant.power_generation)
-    printRowFloat("LCOE", plant.lcoe(tariff,
-                                     discount_rate,
-                                     tax_rate,
-                                     depreciation_period))
-
-
-tableC(MongDuong1System.cofiring_plant.plant, feedin_tariff['MD'])
 print('')
-tableC(MongDuong1System.cofiring_plant, feedin_tariff['MD'], MongDuong1System)
 
-print('')
-
-tableC(NinhBinhSystem.cofiring_plant.plant, feedin_tariff['NB'])
-print('')
-tableC(NinhBinhSystem.cofiring_plant, feedin_tariff['NB'], NinhBinhSystem)
+tableC(NinhBinhSystem, feedin_tariff['NB'])
