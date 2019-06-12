2ba97605d6e4c404980573f8f9ae9abb2885f4e6
Minh Ha-Duong <minh.haduong@vietse.vn>
2019-06-12 15:50:09 +0700

    Define PowerPlant class parent of CoalPowerPlant

--- powerplant.py
+++ powerplant.py
@@ -19,8 +19,70 @@ from investment import Investment
 from emitter import Emitter, Activity
 
 
+class PowerPlant(Investment):
+    """A power plant, without specific technology."""
+
+    def __init__(self,
+                 parameter,
+                 capital=0 * USD):
+        """Initialize the power plant.
+
+        Technology agnostic.
+        """
+        Investment.__init__(self, parameter.name, parameter.time_horizon, capital)
+        self.parameter = parameter
+
+        self.power_generation = (np.ones(parameter.time_horizon + 1) *
+                                 parameter.capacity * parameter.capacity_factor * y)
+        display_as(self.power_generation, 'GWh')
+
+    def operating_expenses(self):
+        cost = self.operation_maintenance_cost()
+        return display_as(cost, 'kUSD')
+
+    def operation_maintenance_cost(self):
+        """Return the vector of operation and maintenance cost."""
+        fixed_om = (np.ones(self.parameter.time_horizon + 1) *
+                         self.parameter.fixed_om * self.parameter.capacity * y)
+        variable_om = self.power_generation * self.parameter.variable_om
+        cost = fixed_om + variable_om
+        return display_as(cost, 'kUSD')
+
+    def lcoe(self, discount_rate, tax_rate, depreciation_period):
+        """Return the levelized cost of electricity, taxes included."""
+        total_lifetime_power_production = npv(discount_rate, self.power_generation)
+        total_life_cycle_cost = npv(discount_rate,
+                                    self.cash_out(tax_rate, depreciation_period))
+        result = total_life_cycle_cost / total_lifetime_power_production
+        return display_as(result, 'USD/MWh')
+
+    def characteristics(self):
+        """Describe the technical characteristics of the plant."""
+        description = pd.Series(name=self.parameter.name)
+        description["Comissioning year"] = self.parameter.commissioning
+        description["Installed capacity"] = self.parameter.capacity
+        return description
+
+    def lcoe_statement(self, discount_rate, tax_rate, depreciation_period):
+        """Assess the levelized cost of electricity."""
+        statement = pd.Series(name=self.name)
+        statement["Investment    (MUSD)"] = self.capital / MUSD
+        statement["O&M cost      (MUSD)"] = npv(
+            discount_rate, self.operation_maintenance_cost()) / MUSD
+        statement["Tax           (MUSD)"] = npv(
+            discount_rate, self.income_tax(tax_rate, depreciation_period)) / MUSD
+        statement["Cash_out      (MUSD)"] = npv(
+            discount_rate,
+            self.cash_out(tax_rate, depreciation_period)) / MUSD
+        statement["Electricity produced"] = npv(
+            discount_rate, self.power_generation)
+        statement["LCOE                "] = self.lcoe(
+            discount_rate, tax_rate, depreciation_period)
+        return statement
+
+
 #pylint: disable=too-many-instance-attributes
-class CoalPowerPlant(Investment, Emitter):
+class CoalPowerPlant(PowerPlant, Emitter):
     """A coal power plant, without co-firing."""
 
     def __init__(self,
@@ -47,12 +109,7 @@ class CoalPowerPlant(Investment, Emitter):
         The capital cost represents the cost of installing cofiring,
         it is zero in this case.
         """
-        Investment.__init__(self, parameter.name, parameter.time_horizon, capital)
-        self.parameter = parameter
-
-        self.power_generation = (np.ones(parameter.time_horizon + 1) *
-                                 parameter.capacity * parameter.capacity_factor * y)
-        display_as(self.power_generation, 'GWh')
+        PowerPlant.__init__(self, parameter, capital)
 
         if derating is not None:
             self.derating = derating
@@ -84,31 +141,18 @@ class CoalPowerPlant(Investment, Emitter):
     def coal_cost(self, value):
         self._coal_cost = value
 
+    def fuel_cost(self):
+        return self.coal_cost
+
     def operating_expenses(self):
         cost = self.fuel_cost() + self.operation_maintenance_cost()
         return display_as(cost, 'kUSD')
-
-    def fuel_cost(self):
-        return self.coal_cost
 
     def operation_maintenance_cost(self):
         return display_as(self.coal_om_cost(), 'kUSD')
 
     def coal_om_cost(self):
-        """Return the vector of operation and maintenance cost."""
-        fixed_om_coal = (np.ones(self.parameter.time_horizon + 1) *
-                         self.parameter.fix_om_coal * self.parameter.capacity * y)
-        variable_om_coal = self.power_generation * self.parameter.variable_om_coal
-        cost = fixed_om_coal + variable_om_coal
-        return display_as(cost, 'kUSD')
-
-    def lcoe(self, discount_rate, tax_rate, depreciation_period):
-        """Return the levelized cost of electricity, taxes included."""
-        total_lifetime_power_production = npv(discount_rate, self.power_generation)
-        total_life_cycle_cost = npv(discount_rate,
-                                    self.cash_out(tax_rate, depreciation_period))
-        result = total_life_cycle_cost / total_lifetime_power_production
-        return display_as(result, 'USD/MWh')
+        return PowerPlant.operation_maintenance_cost(self)
 
     def coal_transport_tkm(self):
         return self.coal_used * 2 * self.parameter.coal.transport_distance   # Return trip inputed
@@ -124,10 +168,8 @@ class CoalPowerPlant(Investment, Emitter):
 
     def characteristics(self):
         """Describe the technical characteristics of the plant."""
-        description = pd.Series(name=self.parameter.name)
-        description["Comissioning year"] = self.parameter.commissioning
+        description = PowerPlant.characteristics(self)
         description["Boiler technology"] = self.parameter.boiler_technology
-        description["Installed capacity"] = self.parameter.capacity
         description["Capacity factor"] = self.parameter.capacity_factor
         description["Coal consumption"] = self.coal_used[1]
         description["Heat value of coal"] = self.parameter.coal.heat_value
@@ -256,12 +298,12 @@ class CofiringPlant(CoalPowerPlant):
     def coal_om_cost(self):  # DISCUSS THIS
         # Fixed costs are proportional to capacity
         fixed_om_coal = ((1 - self.cofire_parameter.biomass_ratio_energy)
-                         * self.parameter.fix_om_coal
+                         * self.parameter.fixed_om
                          * self.parameter.capacity * y)
         # Variable costs proportional to generation after capacity factor
         variable_om_coal = ((1 - self.cofire_parameter.biomass_ratio_energy)
                             * self.power_generation
-                            * self.parameter.variable_om_coal)
+                            * self.parameter.variable_om)
         cost = fixed_om_coal + variable_om_coal
         return display_as(cost, 'kUSD')
 
