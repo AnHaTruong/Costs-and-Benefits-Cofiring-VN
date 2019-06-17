6c274624b451f0fe559405c8429ca1a002226bb6
Minh Ha-Duong <minh.haduong@vietse.vn>
2019-06-17 17:35:30 +0700

    Figure 3 only for Ninh Binh.

--- manuscript1/figure3.py
+++ manuscript1/figure3.py
@@ -9,14 +9,23 @@
 #
 #
 """Draw a figure showing the cumulative costs/benefits for different groups."""
-
 import matplotlib.pyplot as plt
 
 import numpy as np
 
-from model.utils import MUSD
+from model.utils import MUSD, USD, t
+from model.system import System
 from manuscript1.parameters import discount_rate, tax_rate, depreciation_period, external_cost
-from manuscript1.parameters import MongDuong1System, NinhBinhSystem
+from manuscript1.parameters import plant_parameter_NB, cofire_NB, supply_chain_NB, price_NB
+from manuscript1.parameters import farm_parameter, transport_parameter
+
+NinhBinhSystem = System(plant_parameter_NB, cofire_NB, supply_chain_NB, price_NB,
+                        farm_parameter, transport_parameter)
+
+lowPrice = price_NB._replace(biomass=13 * USD / t)
+
+lowPriceSystem = System(plant_parameter_NB, cofire_NB, supply_chain_NB, lowPrice,
+                        farm_parameter, transport_parameter)
 
 
 def benefit_array(system):
@@ -33,24 +42,26 @@ def benefit_array(system):
     farmer_benefit = system.farmer.net_present_value(discount_rate) / MUSD
     health_benefit = system.health_npv(discount_rate, external_cost) / MUSD
     climate_benefit = system.mitigation_npv(discount_rate, external_cost) / MUSD
-    return np.array([plant_benefit, transporter_benefit, farmer_benefit,
-                     job_benefit, health_benefit, climate_benefit])
+    return np.array([transporter_benefit, plant_benefit, farmer_benefit,
+                     job_benefit, climate_benefit, health_benefit])
 
 
 index = np.arange(6)
 width = 0.4
 plt.figure(figsize=(10, 5))
 NB = plt.barh(index + width, benefit_array(NinhBinhSystem), width,
-              color='#ff4500', edgecolor='none', label='Ninh Binh')
-MD = plt.barh(index, benefit_array(MongDuong1System), width,
-              color='navy', edgecolor='none', label='Mong Duong 1')
+              color='#ff4500', edgecolor='none',
+              label='Straw 37.3 USD/t')
+NB2 = plt.barh(index, benefit_array(lowPriceSystem), width,
+               color='navy', edgecolor='none', label='Straw 13 USD/t')
+plt.title('Cofiring 5% straw in Ninh Binh power plant')
 plt.xlabel('Cumulative benefit over 20 years (M$)')
-plt.yticks(index + 0.5, ('Plant owner\n(net profit)',
-                         'Trader\n(transport straw)',
-                         'Farmer\n(sell straw)',
-                         'Workers\n(harvest, transport, O&M)',
-                         'Local society\n(air quality)',
-                         'Global society\n(GHG mitigation)'))
+plt.yticks(index + 0.2, ('Trader profit',
+                         'Plant profit',
+                         'Farmer profit',
+                         'Workers wages\n(harvest, transport, O&M)',
+                         'CO2 emission reduction\nvalued at 1$/tCO2',
+                         'Local air quality\nmostly dust reduction'))
 plt.legend(bbox_to_anchor=(0.98, 0.4), prop={'size': 12}, frameon=False)
 plt.tight_layout()
 
