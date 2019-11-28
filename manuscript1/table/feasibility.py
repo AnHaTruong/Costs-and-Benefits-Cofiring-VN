698a0f277f4337e41374e5e7c45e15515f8f4f9c
Minh Ha-Duong <minh.haduong@vietse.vn>
2019-11-28 16:02:52 +0700

    Separate annual vs. NPV business value.

--- manuscript1/table/feasibility.py
+++ manuscript1/table/feasibility.py
@@ -14,12 +14,23 @@ An Ha Truong, Minh Ha-Duong
 import pandas as pd
 import natu.numpy as np
 
-from model.utils import display_as
+from model.utils import display_as, isclose
 
 from model.wtawtp import farmer_wta, plant_wtp
 
 from manuscript1.parameters import (MongDuong1System,   # NinhBinhSystem,
                                     discount_rate, tax_rate, depreciation_period)
+
+
+row_labels = [
+    'Farmer WTA',
+    'Reseller expenses',
+    'Plant WTP',
+    'Potential gain',
+    'Biomass used per year',
+    'Business value per year',
+    'Biomass used total',
+    'Business value of cofiring']
 
 
 def feasibility_by_solving(system):
@@ -28,60 +39,53 @@ def feasibility_by_solving(system):
     wtp = plant_wtp(system, discount_rate, tax_rate, depreciation_period)
     transport_cost = system.transport_cost_per_t[1]
     potential_gain = wtp - wta - transport_cost
-    q = system.cofiring_plant.biomass_used[1]
-    total_potential = potential_gain * q
+
+    q_per_year = system.cofiring_plant.biomass_used[1]
+    assert isclose(q_per_year, system.farmer.quantity[1])
+    potential_per_year = potential_gain * q_per_year
+    q = np.npv(discount_rate, system.farmer.quantity)
+    business_value = potential_gain * q
 
     data = [
         display_as(wta, "USD/t"),
         display_as(transport_cost, "USD/t"),
         display_as(wtp, "USD/t"),
         display_as(potential_gain, "USD/t"),
-        q,
-        display_as(total_potential, "MUSD")]
+        q_per_year,                  # Changing the display format has too many side effects
+        display_as(potential_per_year, "kUSD"),
+        q,                           # Changing the display format has too many side effects
+        display_as(business_value, "kUSD")]
 
-    index = ['Farmer WTA',
-             'Reseller expenses',
-             'Plant WTP',
-             'Potential gain',
-             'Biomass used',
-             'Total business value']
-
-    return pd.Series(data, index=index, name=system.plant.name + ' by solving')
+    return pd.Series(data, index=row_labels, name=system.plant.name + ' by solve')
 
 
 def feasibility_direct(system):
     """Tabulate the feasibility, using the theoretical analysis."""
     npv_table = system.table_business_value(discount_rate)
-
     q = np.npv(discount_rate, system.farmer.quantity)
 
     wta = npv_table.loc['Farmer opex'] / q
-    display_as(wta, "USD/t")
-
     minimum_margin = npv_table.loc['Transporter opex'] / q
-    display_as(minimum_margin, "USD/t")
-
     investment = npv_table.loc['Investment'] / q
     extra_OM = npv_table.loc['Extra O&M'] / q
     coal_saving = npv_table.loc['Value of coal saved'] / q
     wtp = coal_saving - extra_OM - investment
-    display_as(wtp, "USD/t")
-
     value_per_t = wtp - wta - minimum_margin
-
     value = value_per_t * q
-    display_as(value, "MUSD")
+    q_per_year = system.cofiring_plant.biomass_used[1]
+    potential_per_year = q_per_year * value_per_t
 
-    table = pd.Series(
-        data=[wta, minimum_margin, wtp, value_per_t, q, value],
-        index=["Farmer WTA",
-               "Reseller expenses",
-               "Plant WTP",
-               "Potential gain",
-               "Biomass used",
-               "Total business value"])
-    table.name = system.plant.name + " Direct"
-    return table
+    data = [
+        display_as(wta, "USD/t"),
+        display_as(minimum_margin, "USD/t"),
+        display_as(wtp, "USD/t"),
+        display_as(value_per_t, 'USD/t'),
+        q_per_year,
+        display_as(potential_per_year, 'kUSD'),
+        q,
+        display_as(value, "kUSD")]
+
+    return pd.Series(data, index=row_labels, name=system.plant.name + ' direct')
 
 
 print(pd.concat([
