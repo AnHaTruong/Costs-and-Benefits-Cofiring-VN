ea3edfc79a2d62c1f73ab8d62a182451ffa86396
Minh Ha-Duong <minh.haduong@vietse.vn>
2021-05-20 16:39:36 +0200

    Produce table 2 and 3 for different discount rate and time horizon.

--- bug57.py
+++ bug57.py
@@ -17,7 +17,7 @@ from manuscript1.parameters import (
     depreciation_period,
 )
 
-from model.utils import kUSD, npv, display_as
+from model.utils import kUSD, npv, display_as, TIME_HORIZON
 
 #%% Business value in table
 
@@ -28,101 +28,125 @@ row_labels = [
     "Plant extra O&M",
     "Coal saving",
     "Business value of cofiring",
-    "Biomass used total",
+    "Annual business value of cofiring, levelized",
+    "Annual business value of cofiring, linearized"
+]
+
+
+def table2(system, discount_rate, horizon=(TIME_HORIZON + 1)):
+    """Tabulate the feasibility, using the theoretical analysis.
+
+    Keeping only  horizon  time periods. Investment in period 0, payback starts at 1
+    """
+
+    mask = [1] * horizon + [0] * (TIME_HORIZON + 1 - horizon)
+
+    cost_collect = npv(discount_rate, system.farmer.operating_expenses() * mask)
+
+    minimum_margin = npv(discount_rate, system.reseller.operating_expenses() * mask)
+
+    investment = npv(discount_rate, system.cofiring_plant.investment() * mask)
+
+    extra_OM = npv(discount_rate, system.plant_om_change() * mask)
+
+    coal_saving = npv(discount_rate, system.coal_saved * mask * system.price.coal)
+
+    value = coal_saving - extra_OM - investment - cost_collect - minimum_margin
+
+    value_levelized = value / npv(discount_rate, mask)
+
+    value_linearized = value / (horizon - 1)
+
+    data = [
+        display_as(cost_collect, "kUSD"),
+        display_as(minimum_margin, "kUSD"),
+        display_as(investment, "kUSD"),
+        display_as(extra_OM, "kUSD"),
+        display_as(coal_saving, "kUSD"),
+        display_as(value, "kUSD"),
+        display_as(value_levelized, "kUSD"),
+        display_as(value_linearized, "kUSD"),
+    ]
+
+    result = Series(data, index=row_labels, name=system.plant.name + " table")
+
+    print("Table 2", system.plant.name, "discounted over", horizon, "years at", discount_rate)
+    print(result)
+    print()
+
+
+table2(MongDuong1System, discount_rate)
+table2(MongDuong1System, 0)
+table2(MongDuong1System, 0, 11)
+
+
+#%% Per ton
+
+row_labels = [
+    "Farmer wta",
+    "Reseller average cost",
+    "Plant investment",
+    "Plant extra O&M",
+    "Coal saving",
+    "Plant wtp",
+    "Potential surplus",
+    "Biomass used discounted sum",
+    "Value of cofiring",
     "Biomass used per year",
     "Business value per year",
 ]
 
-def feasibility_direct(system, discount_rate):
+
+def table3(system, discount_rate, horizon=(TIME_HORIZON + 1)):
     """Tabulate the feasibility, using the theoretical analysis."""
 
-    wta =  npv(discount_rate, system.farmer.operating_expenses())
+    mask = [1] * horizon + [0] * (TIME_HORIZON + 1 - horizon)
 
-    minimum_margin = npv(discount_rate, system.reseller.operating_expenses())
+    q = npv(discount_rate, system.farmer.quantity * mask)
 
-    investment = npv(discount_rate, system.cofiring_plant.investment())
+    wta = npv(discount_rate, system.farmer.operating_expenses() * mask) / q
 
-    table_opex = system.plant_npv_opex_change(discount_rate)
-    extra_OM = (table_opex.loc["O&M, main fuel"] + table_opex.loc["O&M, cofuel"] )
-    
-    coal_saving = npv(discount_rate, system.coal_saved * system.price.coal)
+    minimum_margin = npv(discount_rate, system.reseller.operating_expenses() * mask) / q
 
-    value = coal_saving - extra_OM - investment - wta - minimum_margin
+    investment = npv(discount_rate, system.cofiring_plant.investment() * mask) / q
 
-    q = npv(discount_rate, system.farmer.quantity)
+    extra_OM = npv(discount_rate, system.plant_om_change() * mask) / q
 
-    q_per_year = system.quantity_plantgate[1]
- 
-    potential_per_year = q_per_year / q * value
+    coal_saving = npv(discount_rate, system.coal_saved * mask * system.price.coal) / q
+
+    wtp = coal_saving - extra_OM - investment
+
+    surplus = wtp - wta - minimum_margin
+
+    q_per_year = system.farmer.quantity[1]
+
+    potential_per_year = q_per_year * surplus
 
     data = [
-        display_as(wta, "kUSD"),
-        display_as(minimum_margin, "kUSD"),
-        display_as(investment, "kUSD"),
-        display_as(extra_OM, "kUSD"),
-        display_as(coal_saving, "kUSD"),
-        display_as(value, "kUSD"),
-        q,
-        q_per_year,
+        display_as(wta, "USD / t"),
+        display_as(minimum_margin, "USD / t"),
+        display_as(investment, "USD / t"),
+        display_as(extra_OM, "USD / t"),
+        display_as(coal_saving, "USD / t"),
+        display_as(wtp, "USD / t"),
+        display_as(surplus, "USD / t"),
+        display_as(q, "kt"),
+        display_as(surplus * q, "MUSD"),
+        display_as(q_per_year, "kt"),
         display_as(potential_per_year, "kUSD"),
     ]
 
-    return Series(data, index=row_labels, name=system.plant.name + " table")
+    result = Series(data, index=row_labels, name=system.plant.name + " table")
+
+    print("Table 3", system.plant.name, "discounted over", horizon, "years at", discount_rate)
+    print(result)
+    print()
 
 
-table_MD1 = feasibility_direct(MongDuong1System, discount_rate)
-table_NB = feasibility_direct(NinhBinhSystem, discount_rate)
+table3(MongDuong1System, discount_rate)
+table3(MongDuong1System, 0)
+table3(MongDuong1System, 0, 11)  # The one to use
 
-print("From the table, total NPV with discount rate =", discount_rate,":")
-print(concat([table_MD1, table_NB], axis=1))
-
-
-def feasibility_direct_y1(system):
-    """Tabulate the feasibility, undiscounted."""
-
-    wta = system.farmer.operating_expenses()[1]
-    
-    minimum_margin = system.reseller.operating_expenses()[1]
-
-# Suspect the  cause of the difference is amortization
-    investment = system.cofiring_plant.investment()[1]
-
-# FIXME: Cut and paste code for year 1
-    table_opex = system.plant_npv_opex_change(discount_rate)
-    extra_OM = (table_opex.loc["O&M, main fuel"] + table_opex.loc["O&M, cofuel"] )
-    
-    coal_saving = system.coal_saved[1] * system.price.coal
-
-    print(wta, minimum_margin, investment, extra_OM, coal_saving)
-
-    value = coal_saving - extra_OM - investment - wta - minimum_margin
-
-    q = npv(discount_rate, system.farmer.quantity)
-
-    q_per_year = system.quantity_plantgate[1]
- 
-    potential_per_year = q_per_year / q * value
-
-    data = [
-        display_as(wta, "kUSD"),
-        display_as(minimum_margin, "kUSD"),
-        display_as(investment, "kUSD"),
-        display_as(extra_OM, "kUSD"),
-        display_as(coal_saving, "kUSD"),
-        display_as(value, "kUSD"),
-        q,
-        q_per_year,
-        display_as(potential_per_year, "kUSD"),
-    ]
-
-    return Series(data, index=row_labels, name=system.plant.name + " table")
-
-
-print("\nFrom the table, year 1:")
-table_MD1 = feasibility_direct_y1(MongDuong1System)
-table_NB = feasibility_direct_y1(NinhBinhSystem)
-
-print(concat([table_MD1, table_NB], axis=1))
 
 #%% Business value in figure
 
@@ -147,12 +171,11 @@ def cba(system):
         - system.plant.operating_expenses_detail().loc["Operation & Maintenance"]
     )
 
-    data =  [
-            system.farmer.operating_expenses()[1] / kUSD,
+    data = [system.farmer.operating_expenses()[1] / kUSD,
             system.reseller.operating_expenses()[1] / kUSD,
             system.cofiring_plant.amortization(depreciation_period)[1] / kUSD,
             extra_om[1] / kUSD,
-        ]
+            ]
 
     total_costs = sum(data)
     data.append(total_costs)
@@ -171,6 +194,7 @@ def cba(system):
     business_value = fuel_saved[1] / kUSD - total_costs
     data.append(business_value)
     return Series(data, index=row_labels2, name=system.plant.name + " fig")
+
 
 print("\nFrom the figure, year 1 cash flows:")
 
